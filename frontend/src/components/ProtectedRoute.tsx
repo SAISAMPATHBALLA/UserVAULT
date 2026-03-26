bc1df39c5e51281bda6ac90f198471fcaf3fd4d6
SAISAMPATHBALLA <sampath@hivel.ai>
2026-03-26 20:53:49 +0530

    added protected route to the unauth pages and protected route for the auth pages

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom'
+import type { ReactNode } from 'react'
+
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+  const user = localStorage.getItem('user')
+  if (!user) return <Navigate to="/login" replace />
+  return <>{children}</>
+}
+ export const  ProtectedRouteforAuth = ({ children }: { children: ReactNode })=> {
+  const user = localStorage.getItem('user')
+  if (user) return <Navigate to="/dashboard" replace />
+  return <>{children}</>
+}
--- frontend/src/store/MainSlice.ts
+++ frontend/src/store/MainSlice.ts
@@ -4,7 +4,7 @@ export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     value: 0,
-    user : {} as data
+    user : null,
   },
   reducers: {
     increment: (state) => {
