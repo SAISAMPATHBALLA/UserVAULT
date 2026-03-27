95b655b282652e06d2dcccc6f3d887c0f39e18b5
SAISAMPATHBALLA <sampath@hivel.ai>
2026-03-27 11:23:49 +0530

    added new user filters and the hooks to fetch user's posts, carts, todos

--- frontend/src/apis/getUserDetails.ts
+++ frontend/src/apis/getUserDetails.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Users, UsersResponse, User } from '../types/Users'
 import { API_BASE_URL } from '../constants/authConstants'
-
+import type { CartsResponse } from '../types/UserCart'
+import type { PostsResponse } from '../types/UserPosts'
+import type { TodosResponse } from '../types/UserTodos'
 export const usersDetailsApi = createApi({
   reducerPath: 'userFetchApi',
   baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
@@ -23,6 +25,12 @@ export const usersDetailsApi = createApi({
         },
       }),
     }),
+    UsersByHairColor: build.query<UsersResponse, string>({
+      query: (hairColor) => ({
+        url: `users/filter?key=hair.color&value=${hairColor}`,
+        method: 'GET',
+      }),
+    }),
     searchUsers: build.query<UsersResponse, string>({
       query: (q) => ({
         url: 'users/search',
@@ -30,8 +38,18 @@ export const usersDetailsApi = createApi({
         params: { q },
       }),
     }),
+
     getUserById: build.query<User, number>({
       query: (id) => ({ url: `users/${id}` }),
+    }),
+    getUserCartsById: build.query<CartsResponse, number>({
+      query: (id) => ({ url: `users/${id}/carts` }),
+    }),
+    getUserPostsById: build.query<PostsResponse, number>({
+      query: (id) => ({ url: `users/${id}/posts` }),
+    }),
+    getUserTodosById: build.query<TodosResponse, number>({
+      query: (id) => ({ url: `users/${id}/todos` }),
     }),
   }),
 })
@@ -41,4 +59,8 @@ export const {
   useGetUsersQuery,
   useSearchUsersQuery,
   useGetUserByIdQuery,
+  useUsersByHairColorQuery,
+  useGetUserCartsByIdQuery,
+  useGetUserPostsByIdQuery,
+  useGetUserTodosByIdQuery,
 } = usersDetailsApi
